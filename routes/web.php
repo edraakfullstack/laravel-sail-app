b8858793d811efbda8602a6fad9993a149d004ac
EngIsmail2058 <107861514+EngIsmail2058@users.noreply.github.com>
2022-10-28 16:31:53 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function ('Stag') {
     return view('mepage');
 });
 
-Route::get('/Stag', function () {
+Route::get('/', function () {
     return view('Stag');
 });
 
